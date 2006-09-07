60d82c8a73d0d81beaee2399b7af5fd52b69bf5c
Tino Didriksen <mail@tinodidriksen.com>
2006-09-07 17:14:14 +0000

    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@1548 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-g++ -combine -O3 -Wall -ffor-scope -ffast-math -floop-optimize -fexpensive-optimizations -fmove-loop-invariants -mmmx -msse -licuio -licuuc src/*.cpp -o vislcg3
+g++ -O3 -Wall -ffor-scope -ffast-math -fexpensive-optimizations -licuio -licuuc src/*.cpp -o vislcg3
 strip vislcg3
