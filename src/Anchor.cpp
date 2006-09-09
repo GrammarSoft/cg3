21ef385e93f91491a0268837ad69a816b4da13af
Tino Didriksen <mail@tinodidriksen.com>
2006-09-09 00:42:28 +0000

    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@1559 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ src/Anchor.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2006, GrammarSoft Aps
+ * and the VISL project at the University of Southern Denmark.
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the GrammarSoft Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.grammarsoft.com/GSPL or
+ * http://visl.sdu.dk/GSPL.txt
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+#include "stdafx.h"
+#include <unicode/ustring.h>
+#include "Anchor.h"
+
+using namespace CG3;
+
+Anchor::Anchor() {
+	name = 0;
+	line = 0;
+}
+
+Anchor::~Anchor() {
+	if (name) {
+		delete name;
+	}
+	line = 0;
+}
+
+void Anchor::setName(uint32_t to) {
+	if (!to) {
+		to = (uint32_t)rand();
+	}
+	name = new UChar[32];
+	memset(name, 0, 32);
+	u_sprintf(name, "_G_%u_%u_", line, to);
+//	u_sprintf(name, "_G_%u_", to);
+}
+void Anchor::setName(const UChar *to) {
+	if (to) {
+		name = new UChar[u_strlen(to)+1];
+		u_strcpy(name, to);
+	} else {
+		setName((uint32_t)rand());
+	}
+}
---
+++ src/Anchor.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2006, GrammarSoft Aps
+ * and the VISL project at the University of Southern Denmark.
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the GrammarSoft Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.grammarsoft.com/GSPL or
+ * http://visl.sdu.dk/GSPL.txt
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+#ifndef __ANCHOR_H
+#define __ANCHOR_H
+
+#include <unicode/ustdio.h>
+#include <unicode/ustring.h>
+
+namespace CG3 {
+
+	class Anchor {
+	public:
+		UChar *name;
+		uint32_t line;
+
+		Anchor();
+		~Anchor();
+
+		void setName(const UChar *to);
+		void setName(uint32_t to);
+	};
+
+}
+
+#endif
