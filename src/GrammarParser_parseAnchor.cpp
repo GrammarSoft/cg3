f2ef4a74017fef0ae8701c90187de2e2b04a7cfc
Tino Didriksen <mail@tinodidriksen.com>
2006-09-13 02:12:37 +0000

    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@1569 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ src/GrammarParser_parseAnchor.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2006, GrammarSoft Aps
+ * and the VISL project at the University of Southern Denmark.
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the GrammarSoft Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.grammarsoft.com/GSPL or
+ * http://visl.sdu.dk/GSPL.txt
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+#include "stdafx.h"
+#include "Strings.h"
+#include <unicode/uregex.h>
+#include "GrammarParser.h"
+#include "Grammar.h"
+#include "uextras.h"
+
+using namespace CG3;
+using namespace CG3::Strings;
+
+int GrammarParser::parseAnchor(const UChar *line) {
+	if (!line) {
+		u_fprintf(ux_stderr, "Error: No string provided at line %u - cannot continue!\n", result->curline);
+		return -1;
+	}
+	int length = u_strlen(line);
+	if (!length) {
+		u_fprintf(ux_stderr, "Error: No string provided at line %u - cannot continue!\n", result->curline);
+		return -1;
+	}
+	uint32_t nline = (uint32_t)result->rules.size();
+
+	const UChar *space = u_strchr(line, ' ');
+	if (space) {
+		space++;
+		result->addAnchor(space, nline);
+	}
+	else {
+		u_fprintf(ux_stderr, "Error: No anchor name provided at line %u!\n", result->curline);
+		return -1;
+	}
+
+	return 0;
+}
---
+++ src/GrammarParser_parseSection.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2006, GrammarSoft Aps
+ * and the VISL project at the University of Southern Denmark.
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the GrammarSoft Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.grammarsoft.com/GSPL or
+ * http://visl.sdu.dk/GSPL.txt
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+#include "stdafx.h"
+#include "Strings.h"
+#include <unicode/uregex.h>
+#include "GrammarParser.h"
+#include "Grammar.h"
+#include "uextras.h"
+
+using namespace CG3;
+using namespace CG3::Strings;
+
+int GrammarParser::parseSection(const UChar *line) {
+	if (!line) {
+		u_fprintf(ux_stderr, "Error: No string provided at line %u - cannot continue!\n", result->curline);
+		return -1;
+	}
+	int length = u_strlen(line);
+	if (!length) {
+		u_fprintf(ux_stderr, "Error: No string provided at line %u - cannot continue!\n", result->curline);
+		return -1;
+	}
+	uint32_t nline = (uint32_t)result->rules.size();
+	result->sections.push_back(nline);
+
+	const UChar *space = u_strchr(line, ' ');
+	if (space) {
+		space++;
+		result->addAnchor(space, nline);
+	}
+
+	return 0;
+}
