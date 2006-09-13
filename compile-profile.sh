dd28df188adbfdb70a09f2a18c20803a54843c00
Tino Didriksen <mail@tinodidriksen.com>
2006-09-13 03:56:19 +0000

    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@1571 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-profile.sh
+++ compile-profile.sh
@@ -1,1 +1,7 @@
-g++ -Wall -O0 -g3 -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc src/*.cpp -o vislcg3
+mkdir -p gcov
+cd gcov
+g++ -Wall -O0 -g3 -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc ../src/*.cpp -o vislcg3
+cd ..
+mkdir -p gprof
+cd gprof
+g++ -pg -Wall -O0 -g3 -ffor-scope -licuio -licuuc ../src/*.cpp -o vislcg3
