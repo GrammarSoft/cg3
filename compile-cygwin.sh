1edcd6971912d311c7b2b768825bed666bdb2bcf
Tino Didriksen <mail@tinodidriksen.com>
2006-10-10 18:44:13 +0000

    Split up compile.sh into Linux, Mac OS X, and Cygwin versions. Right now, only Linux is known to work.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@1669 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ compile-cygwin.sh
@@ -0,0 +1,2 @@
+g++ -O2 -Wall -ffor-scope -ffast-math -fexpensive-optimizations -licuio -licuuc -licuin src/*.cpp -o vislcg3.exe
+strip vislcg3.exe
---
+++ compile-mac.sh
@@ -0,0 +1,2 @@
+g++ -O3 -Wall -ffor-scope -ffast-math -fexpensive-optimizations -licuio -licuuc -licuin src/*.cpp -o vislcg3
+strip vislcg3
