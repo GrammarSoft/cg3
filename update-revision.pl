78fc330105fc917a4a8e1fb7279186b720548ce9
Tino Didriksen <mail@tinodidriksen.com>
2007-04-02 15:24:31 +0000

    Documented rule application order.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@2055 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- update-revision.pl
+++ update-revision.pl
@@ -2,7 +2,11 @@
 use strict;
 use warnings;
 
+print `svn up`;
 my $revision = `svnversion -n`;
 $revision =~ s/^([0-9]+).*/$1/g;
+$revision += 1;
 
 `/usr/bin/perl -e 's/CG3_REVISION [0-9]+\$/CG3_REVISION $revision/;' -pi src/version.h`;
+
+print "Set revision to $revision.\n";
