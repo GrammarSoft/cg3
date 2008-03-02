8443f5fe0cde61b8365d4edd9399e331d6ddf204
Tino Didriksen <mail@tinodidriksen.com>
2008-03-02 17:26:59 +0000

    Added an automatic fetch&compile&test for Linux+wget.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3264 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ scripts/auto-linux-wget.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Cleaning up /tmp/vislcg3-auto ..."
+cd /tmp/
+rm -rfv vislcg3-auto
+mkdir vislcg3-auto
+cd vislcg3-auto
+
+echo "Fetching VISL CG-3 source with wget..."
+wget -nv -r -l0 -np -nH --http-user=anonymous --http-password=anonymous http://beta.visl.sdu.dk/svn/visl/tools/vislcg3/
+cd svn/visl/tools/vislcg3/trunk/
+chmod +x *.sh test/*.pl test/*/*.pl
+
+echo "Fetched everything...compiling..."
+./compile-linux.sh
+
+echo "Compiled. Running regression tests..."
+./test/runall.pl
+
+echo "If all normal tests were successful, you can now do:"
+echo "cp $(pwd)/vislcg3 /usr/local/bin/"
