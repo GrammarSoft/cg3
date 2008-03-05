5772c2b4fefeee196dfd40cc84e9bd8f9b24f74e
Tino Didriksen <mail@tinodidriksen.com>
2008-03-05 14:49:14 +0000

    Defaulted to TextualParser; Enabled previous with --old-parser.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3277 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- test/T_SoftDelimiters/run.pl
+++ test/T_SoftDelimiters/run.pl
@@ -12,7 +12,7 @@ if (!$binary || $binary eq '' || !(-x $binary)) {
 	die("Error: $binary is not executable!");
 }
 
-`"$binary" -C ISO-8859-1 --soft-limit 4 --hard-limit 6 --grammar grammar.txt -I input.txt -O output.txt >stdout.txt 2>stderr.txt`;
+`"$binary" $ARGV[1] -C ISO-8859-1 --soft-limit 4 --hard-limit 6 --grammar grammar.txt -I input.txt -O output.txt >stdout.txt 2>stderr.txt`;
 `diff -B expected.txt output.txt >diff.txt`;
 
 if (-s "diff.txt") {
