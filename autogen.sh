025423f1b6e85acf45f1ad6de5380bfd8f109a9c
Francis Tyers <ftyers@prompsit.com>
2008-04-13 12:31:11 +0000

    Minor changes to autotools build to remove autoheader stuff
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3421 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,6 @@ echo "- aclocal."               && \
 aclocal -I m4                   && \
 echo "- autoconf."              && \
 autoconf                        && \
-echo "- autoheader."            && \
-autoheader                      && \
 echo "- automake."              && \
 automake --add-missing --gnu    && \
 echo                            && \
