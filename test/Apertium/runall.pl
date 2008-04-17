cccfadcb2eb9f855be343f369e98981fd5e47a23
Francis Tyers <ftyers@prompsit.com>
2008-04-17 21:09:54 +0000

    Adding tests from Apertium branch
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3522 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ test/Apertium/runall.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Cwd qw(realpath);
+
+my ($bindir, $sep) = $0 =~ /^(.*)(\\|\/).*/;
+$bindir = realpath $bindir;
+chdir $bindir or die("Error: Could not change directory to $bindir !");
+
+# Search paths for the binary
+my @binlist = (
+	"../Debug/vislcg3",
+	"../Release/vislcg3",
+	"../vislcg3",
+);
+my $binary = "vislcg3";
+
+foreach (@binlist) {
+	if (-x $_) {
+		$binary = $_;
+		last;
+	}
+	elsif (-x $_.".exe") {
+		$binary = $_.".exe";
+		last;
+	}
+}
+$binary = realpath $binary;
+print STDERR "Binary found at: $binary\n";
+
+print STDERR "\nRunning tests...\n";
+
+my @tests = grep { -x } glob('./T*/run.pl');
+foreach (@tests) {
+	if ($ARGV[0] && $ARGV[0] ne "" && !(/$ARGV[0]/i)) {
+		next;
+	}
+	chdir $bindir or die("Error: Could not change directory to $bindir !");
+	my ($test) = m/^.*?(T[^\/]+).*$/;
+	print STDERR "$test: ";
+	if (-s "./$test/byline.txt") {
+		print STDERR "(".`cat "./$test/byline.txt"`.") ";
+	}
+	if (-e "./".$test."/diff.txt") {
+	    unlink "./".$test."/diff.txt";
+	}
+	if (-e "./".$test."/output.txt") {
+	    unlink "./".$test."/output.txt";
+	}
+	my $c = '""';
+	if ($ARGV[1] && $ARGV[1] ne "") {
+		$c = '"'.$ARGV[1].'"';
+	}
+	`$_ "$binary" \Q$c\E`;
+}
+
+print STDERR "\n";
