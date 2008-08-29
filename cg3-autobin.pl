94d74ee5e6008e72db3ddc1711d935df0beab885
Tino Didriksen <mail@tinodidriksen.com>
2008-08-29 11:46:25 +0000

    Added automatic binarizing wrapper cg3-autobin.pl
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3906 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ cg3-autobin.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use File::Spec;
+use Getopt::Long;
+use Digest::SHA qw(sha1_hex);
+
+# Generate list with:
+# vislcg3 --help 2>&1 | perl -wpne 'if (/^ / && /-(\w), --([-\w]+)/) {print "$2|$1=s\n"} elsif (/^ / && /--([-\w]+)/) {print "$1=s\n"} s/^.*$//s;' | perl -wpne 's/^/"/; s/$/",/;'
+my %h = ();
+GetOptions (\%h,
+"help|h",
+"version|V",
+"grammar|g=s",
+"grammar-out=s",
+"grammar-bin=s",
+"grammar-info=s",
+"grammar-only",
+"check-only",
+"sections|s=s",
+"verbose|v=s",
+"vislcg-compat|2",
+"stdin|I=s",
+"stdout|O=s",
+"stderr|E=s",
+"codepage-all|C=s",
+"codepage-grammar=s",
+"codepage-input=s",
+"codepage-output=s",
+"locale-all|L=s",
+"locale-grammar=s",
+"locale-input=s",
+"locale-output=s",
+"no-mappings",
+"no-corrections",
+"no-before-sections",
+"no-sections",
+"no-after-sections",
+"trace|t",
+"trace-name-only",
+"trace-no-removed",
+"single-run",
+"prefix|p=s",
+"num-windows=s",
+"always-span",
+"soft-limit=s",
+"hard-limit=s",
+"dep-allow-loops",
+"no-magic-readings",
+"no-pass-origin|o"
+);
+
+if (defined $h{'grammar-bin'}) {
+	die "Error: Cannot use --grammar-bin with the autobin wrapper !\n";
+}
+
+if (! defined $h{grammar}) {
+	die "Error: Missing --grammar or -g !\n";
+}
+my $grammar = $h{grammar};
+
+if (! (-r $h{grammar})) {
+	die "Error: Cannot read file ".$h{grammar}." !\n";
+}
+
+my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($h{grammar});
+
+my $bn = File::Spec->tmpdir()."/".sha1_hex($h{grammar}).".".$mtime.".bin3";
+
+my $args = " ";
+while (my ($k,$v) = each(%h)) {
+	if ($k ne 'grammar') {
+		$args .= " --$k $v";
+	}
+}
+
+if (!(-r $bn)) {
+	`vislcg3 $args --grammar $grammar --grammar-only --grammar-bin $bn`;
+}
+
+if (-r $bn) {
+	print STDERR "CG3 AutoBin using $bn\n";
+	`cat | vislcg3 $args --grammar $bn`;
+}
+else {
+	print STDERR "CG3 AutoBin failed - falling back to normal\n";
+	`cat | vislcg3 $args --grammar $grammar`;
+}
