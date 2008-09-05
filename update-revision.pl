e5290051eea5c351313e43cf9e02f29211d76073
Tino Didriksen <mail@tinodidriksen.com>
2008-09-05 14:42:08 +0000

    Added revision to autobin's tmp name.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@3962 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- update-revision.pl
+++ update-revision.pl
@@ -8,5 +8,6 @@ $revision =~ s/^([0-9]+).*/$1/g;
 $revision += 1;
 
 `/usr/bin/perl -e 's/CG3_REVISION [0-9]+\$/CG3_REVISION $revision/;' -pi src/version.h`;
+`/usr/bin/perl -e 's/\$revision = [0-9]+;\$/\$revision = $revision;/;' -pi scripts/cg3-autobin.pl`;
 
 print "Set revision to $revision.\n";
