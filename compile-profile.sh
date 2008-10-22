e2414c0c83907a13e0af31f62d0669963d0ee85a
Tino Didriksen <mail@tinodidriksen.com>
2008-10-22 23:02:59 +0000

    Un-optimize for profiling
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@4204 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-profile.sh
+++ compile-profile.sh
@@ -1,9 +1,13 @@
 mkdir -p gcov
-rm -f gcov/*
+rm -f gcov/*.gcov
+rm -f gcov/*.gcda
+rm -f gcov/*.gcno
+rm -f gcov/vislcg3
 cd gcov
-g++ -Wall -O2 -g3 -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
+g++ -Wall -O -g3 -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
 cd ..
 mkdir -p gprof
-rm -f gprof/*
+rm -f gprof/vislcg3
+rm -f gprof/gmon.out
 cd gprof
-g++ -pg -Wall -O2 -g3 -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
+g++ -pg -Wall -O -g3 -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
