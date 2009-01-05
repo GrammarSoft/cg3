3d66b53f99571d622508c4663a6ff4167ac41766
Tino Didriksen <mail@tinodidriksen.com>
2009-01-05 11:51:18 +0000

    Test
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@4403 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-mac.sh
+++ compile-mac.sh
@@ -1,4 +1,4 @@
 echo "NOTICE: This script is deprecated."
 echo "Instead, run autogen.sh or configure, then make, ./test/runall.pl, and if Success, make install."
-g++ -O2 -Wall -ffor-scope -ffast-math -fexpensive-optimizations -licuio -licuuc -licui18n -licudata $(ls -1 ./src/*.cpp | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
+g++ -O2 -Wall -ffor-scope -ffast-math -fexpensive-optimizations -licuio -licuuc -licui18n -licudata $(ls -1 ./src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
 strip vislcg3
