50f5cadd51e7c09e85158091719028ae9b5d8a89
Tino Didriksen <mail@tinodidriksen.com>
2009-03-27 16:14:29 +0000

    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@4654 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/test_hash_arch.cpp
+++ src/test_hash_arch.cpp
@@ -1,9 +1,46 @@
+/*
+* Copyright (C) 2007, GrammarSoft ApS
+* Developed by Tino Didriksen <tino@didriksen.cc>
+* Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <tino@didriksen.cc>
+*
+* This file is part of VISL CG-3
+*
+* VISL CG-3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* VISL CG-3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with VISL CG-3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "stdafx.h"
 
 int main(int argc, char* argv[]) {
-	char *buffer = "letters go here";
-	
-	printf("Hash: %u\n", hash_sdbm_char(buffer));
-	
-	return 0;
+	UErrorCode status = U_ZERO_ERROR;
+
+	/* Initialize ICU */
+	u_init(&status);
+	if (U_FAILURE(status) && status != U_FILE_ACCESS_ERROR) {
+		std::cerr << "Error: Cannot initialize ICU. Status = " << u_errorName(status) << std::endl;
+		CG3Quit(1);
+	}
+	status = U_ZERO_ERROR;
+
+	ucnv_setDefaultName("UTF-8");
+	uloc_setDefault("en_US_POSIX", &status);
+
+	UChar *t = new UChar[512];
+	memset(t, 0, sizeof(UChar)*512);
+	u_sprintf(t, "waffles with pancakes");
+	printf("Hash: %u\n", hash_sdbm_uchar(t));
+
+	u_cleanup();
+
+	return status;
 }
