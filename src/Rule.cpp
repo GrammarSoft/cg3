b479ecfa7ce2e40cfee8a2880d03540f30922907
Tino Didriksen <mail@tinodidriksen.com>
2009-09-23 23:13:32 +0000

    cppcheck cleanup
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5107 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/Rule.cpp
+++ src/Rule.cpp
@@ -65,9 +65,8 @@ Rule::~Rule() {
 }
 
 void Rule::setName(const UChar *to) {
-	if (name) {
-		delete[] name;
-	}
+	delete[] name;
+	name = 0;
 	if (to) {
 		name = new UChar[u_strlen(to)+1];
 		u_strcpy(name, to);
