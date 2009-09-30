07bbc378675a4defad4f9963fda1cadb63c9e5a7
Tino Didriksen <mail@tinodidriksen.com>
2009-09-30 21:19:04 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5134 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ src/CohortIterator.h
@@ -0,0 +1,34 @@
+/*
+* Copyright (C) 2007, GrammarSoft ApS
+* Developed by Tino Didriksen <tino@didriksen.cc>
+* Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <tino@didriksen.cc>
+*
+* This file is part of VISL CG-3
+*
+* VISL CG-3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* VISL CG-3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with VISL CG-3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef __COHORTITERATOR_H
+#define __COHORTITERATOR_H
+
+#include "stdafx.h"
+#include "Cohort.h"
+
+namespace CG3 {
+	class CohortIterator : public std::iterator<std::input_iterator_tag, Cohort*> {
+	};
+}
+
+#endif
