1ae31affc1cab4c8533b2fe4d6dd7198fdcf6145
Tino Didriksen <mail@tinodidriksen.com>
2009-10-30 14:17:01 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5233 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-profile.sh
+++ compile-profile.sh
@@ -4,10 +4,10 @@ rm -f gcov/*.gcda
 rm -f gcov/*.gcno
 rm -f gcov/vislcg3
 cd gcov
-g++ -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
+g++ -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin) -o vislcg3
 cd ..
 mkdir -p gprof
 rm -f gprof/vislcg3
 rm -f gprof/gmon.out
 cd gprof
-g++ -pg -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium) -o vislcg3
+g++ -pg -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin) -o vislcg3
