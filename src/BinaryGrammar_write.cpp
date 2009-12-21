4a556c40b41b05dd1077a565623e3e34c1b202a8
Tino Didriksen <mail@tinodidriksen.com>
2009-12-21 18:42:13 +0000

    Fixed bug where barriers for overridden templates where not saved in binary format; Bumped binary revision.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5459 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/BinaryGrammar_write.cpp
+++ src/BinaryGrammar_write.cpp
@@ -364,17 +364,18 @@ void BinaryGrammar::writeContextualTest(ContextualTest *t, FILE *output) {
 		u32tmp = 0;
 		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
 
-		u32tmp = (uint32_t)htonl((uint32_t)t->line);
-		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
 		u32tmp = (uint32_t)htonl((uint32_t)t->target);
 		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
-		u32tmp = (uint32_t)htonl((uint32_t)t->relation);
-		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
-		u32tmp = (uint32_t)htonl((uint32_t)t->barrier);
-		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
-		u32tmp = (uint32_t)htonl((uint32_t)t->cbarrier);
-		fwrite(&u32tmp, sizeof(uint32_t), 1, output);
 	}
+
+	u32tmp = (uint32_t)htonl((uint32_t)t->line);
+	fwrite(&u32tmp, sizeof(uint32_t), 1, output);
+	u32tmp = (uint32_t)htonl((uint32_t)t->relation);
+	fwrite(&u32tmp, sizeof(uint32_t), 1, output);
+	u32tmp = (uint32_t)htonl((uint32_t)t->barrier);
+	fwrite(&u32tmp, sizeof(uint32_t), 1, output);
+	u32tmp = (uint32_t)htonl((uint32_t)t->cbarrier);
+	fwrite(&u32tmp, sizeof(uint32_t), 1, output);
 
 	if (t->linked) {
 		u8tmp = (uint8_t)1;
