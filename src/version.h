25d3483463ae993cfc21e2defa8091f9f69168fb
Tino Didriksen <mail@tinodidriksen.com>
2010-03-24 16:52:06 +0000

    Revision bump
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5773 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@ const char CG3_COPYRIGHT_STRING[] = "Copyright (C) 2007-2010 GrammarSoft ApS. Al
 const uint32_t CG3_VERSION_MAJOR = 0;
 const uint32_t CG3_VERSION_MINOR = 9;
 const uint32_t CG3_VERSION_PATCH = 7;
-const uint32_t CG3_REVISION = 5729;
+const uint32_t CG3_REVISION = 5773;
 const uint32_t CG3_TOO_OLD = 5465;
 
 #endif
