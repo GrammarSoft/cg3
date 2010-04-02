6d18dac3e0e91027956e27a74f0e88dbf911a820
Tino Didriksen <mail@tinodidriksen.com>
2010-04-02 00:34:34 +0000

    Eliminated doesTagMatchSet() in favour of doing a full doesSetMatchCohortNormal() for testing delimiters.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@5828 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/MatxinApplicator.cpp
+++ src/MatxinApplicator.cpp
@@ -219,13 +219,13 @@ int MatxinApplicator::runGrammarOnText(UFILE *input, UFILE *output) {
 		}
 			
 		if (incohort) {
+			// Create magic reading
+			if (cCohort && cCohort->readings.empty()) {
+				cReading = initEmptyCohort(*cCohort);
+				lReading = cReading;
+			}
 			if (cCohort && cSWindow->cohorts.size() >= soft_limit && grammar->soft_delimiters && doesSetMatchCohortNormal(*cCohort, grammar->soft_delimiters->hash)) {
 			  // ie. we've read some cohorts
-				// Create magic reading
-				if (cCohort->readings.empty()) {
-					cReading = initEmptyCohort(*cCohort);
-					lReading = cReading;
-				}
 				foreach (ReadingList, cCohort->readings, iter, iter_end) {
 					addTagToReading(**iter, endtag);
 				}
@@ -242,11 +242,6 @@ int MatxinApplicator::runGrammarOnText(UFILE *input, UFILE *output) {
 					u_fprintf(ux_stderr, "Warning: Hard limit of %u cohorts reached at line %u - forcing break.\n", hard_limit, numLines);
 					u_fflush(ux_stderr);
 				}
-				// Create magic reading
-				if (cCohort->readings.empty()) {
-					cReading = initEmptyCohort(*cCohort);
-					lReading = cReading;
-				} 
 				foreach (ReadingList, cCohort->readings, iter, iter_end) {
 					addTagToReading(**iter, endtag);
 				}
@@ -293,11 +288,6 @@ int MatxinApplicator::runGrammarOnText(UFILE *input, UFILE *output) {
 			if (cCohort && cSWindow) {
 				cSWindow->appendCohort(cCohort);
 				lCohort = cCohort;
-				// Create readings
-				if (cCohort->readings.empty()) {
-					cReading = initEmptyCohort(*cCohort);
-					lReading = cReading;
-				}
 			} 
 			if (gWindow->next.size() > num_windows) {
 				while (!gWindow->previous.empty() && gWindow->previous.size() > num_windows) {
