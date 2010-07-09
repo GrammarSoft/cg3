b247f55ccd8bf50156fb663b99d719d63bb73c72
Tino Didriksen <mail@tinodidriksen.com>
2010-07-09 16:57:39 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@6248 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-profile.sh
+++ compile-profile.sh
@@ -4,10 +4,10 @@ rm -fv gcov/*.gcda
 rm -fv gcov/*.gcno
 rm -fv gcov/vislcg3
 cd gcov
-g++ -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin | grep -v FormatConverter) -o vislcg3
+g++ -DHAVE_BOOST -I~/tmp/boost_1_42_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin | grep -v FormatConverter) -o vislcg3
 cd ..
 mkdir -p gprof
 rm -fv gprof/vislcg3
 rm -fv gprof/gmon.out
 cd gprof
-g++ -pipe -pg -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin | grep -v FormatConverter) -o vislcg3
+g++ -DHAVE_BOOST -I~/tmp/boost_1_42_0 -pipe -pg -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -licuio -licuuc $(ls -1 ../src/*.cpp | egrep -v '/test_' | egrep -v '/cg_' | grep -v Apertium | grep -v Matxin | grep -v FormatConverter) -o vislcg3
