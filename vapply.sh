cd11ca2745bd5058201e94e1f005321cd54ba371
Tino Didriksen <mail@tinodidriksen.com>
2011-01-21 22:54:43 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@6636 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- vapply.sh
+++ vapply.sh
@@ -9,4 +9,4 @@ rm -fv annotated
 g++ -DHAVE_BOOST -I~/tmp/boost_1_44_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
 head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -v -C UTF-8 -g ../dancg.bin3 > output.txt
 #head -n 2000 ../comparison/arboretum_stripped.txt | valgrind ./vislcg3.debug -v -C UTF-8 -g ../dancg.bin3 > output.txt
-callgrind_annotate --tree=both > annotated
+callgrind_annotate --tree=both --auto=yes > annotated
