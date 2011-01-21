824b150c55dd4b3a91f7a42327a6c4974812a2fc
Tino Didriksen <mail@tinodidriksen.com>
2011-01-21 22:54:43 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@6636 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- vparse.sh
+++ vparse.sh
@@ -4,6 +4,6 @@ mkdir -p vparse
 cd vparse
 rm -f callgrind.*
 rm -f annotated
-g++ -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
-valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -C ISO-8859-1 --grammar-only -g ~/parsers/dansk/etc/dancg
-callgrind_annotate --tree=both > annotated
+g++ -DHAVE_BOOST -I~/tmp/boost_1_44_0 -Wall -Wextra -Wno-deprecated -pipe -O3 -g3 -fno-rtti -ffor-scope -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
+valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -C UTF-8 --grammar-only -g ~/parsers/dansk/etc/dancg
+callgrind_annotate --tree=both --auto=yes > annotated
