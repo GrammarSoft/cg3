c53fb9edf14c409f596d5d7748f0ea4a42a43f58
Tino Didriksen <mail@tinodidriksen.com>
2011-05-09 21:29:16 +0000

    More library
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@7128 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/cg3.h
+++ src/cg3.h
@@ -31,71 +31,70 @@
 extern "C" {
 #endif
 
-#ifndef _CG3_INTERNAL
-	typedef struct _cg3_grammar_t    cg3_grammar_t;
-	typedef struct _cg3_applicator_t cg3_applicator_t;
-	typedef struct _cg3_sentence_t   cg3_sentence_t;
-	typedef struct _cg3_cohort_t     cg3_cohort_t;
-	typedef struct _cg3_reading_t    cg3_reading_t;
-	typedef struct _cg3_tag_t        cg3_tag_t;
-#endif
+typedef void cg3_grammar;
+typedef void cg3_applicator;
+typedef void cg3_sentence;
+typedef void cg3_cohort;
+typedef void cg3_reading;
+typedef void cg3_tag;
 
-enum cg3_status_t {
+typedef enum {
 	CG3_ERROR   = 0,
 	CG3_SUCCESS = 1,
-};
+} cg3_status;
 
 // Default usage: if (!cg3_init(stdin, stdout, stderr)) { exit(1); }
-cg3_status_t cg3_init(FILE *in, FILE *out, FILE *err);
+cg3_status cg3_init(FILE *in, FILE *out, FILE *err);
 // Default usage: cg3_cleanup();
-cg3_status_t cg3_cleanup(void);
+cg3_status cg3_cleanup(void);
 
-cg3_grammar_t *cg3_grammar_load(const char *filename);
-void cg3_grammar_free(cg3_grammar_t *grammar);
+cg3_grammar *cg3_grammar_load(const char *filename);
+void cg3_grammar_free(cg3_grammar *grammar);
 
-cg3_applicator_t *cg3_applicator_create(cg3_grammar_t *grammar);
-void cg3_applicator_free(cg3_applicator_t *applicator);
+cg3_applicator *cg3_applicator_create(cg3_grammar *grammar);
+void cg3_applicator_free(cg3_applicator *applicator);
 
-cg3_sentence_t *cg3_sentence_new(cg3_applicator_t *applicator);
-void cg3_sentence_runrules(cg3_applicator_t *applicator, cg3_sentence_t *sentence);
+cg3_sentence *cg3_sentence_new(cg3_applicator *applicator);
+void cg3_sentence_runrules(cg3_applicator *applicator, cg3_sentence *sentence);
 // The Sentence takes ownership of the Cohort here.
-void cg3_sentence_addcohort(cg3_sentence_t *sentence, cg3_cohort_t *cohort);
-size_t cg3_sentence_numcohorts(cg3_sentence_t *sentence);
-cg3_cohort_t *cg3_sentence_getcohort(cg3_sentence_t *sentence, size_t which);
-void cg3_sentence_free(cg3_sentence_t *sentence);
+void cg3_sentence_addcohort(cg3_sentence *sentence, cg3_cohort *cohort);
+size_t cg3_sentence_numcohorts(cg3_sentence *sentence);
+cg3_cohort *cg3_sentence_getcohort(cg3_sentence *sentence, size_t which);
+void cg3_sentence_free(cg3_sentence *sentence);
 
-cg3_cohort_t *cg3_cohort_create(cg3_sentence_t *sentence);
-void cg3_cohort_setwordform(cg3_cohort_t *cohort, cg3_tag_t *wordform);
-void cg3_cohort_setdependency(cg3_cohort_t *cohort, uint32_t dep_self, uint32_t dep_parent);
+cg3_cohort *cg3_cohort_create(cg3_sentence *sentence);
+void cg3_cohort_setwordform(cg3_cohort *cohort, cg3_tag *wordform);
+cg3_tag *cg3_cohort_getwordform(cg3_cohort *cohort);
+void cg3_cohort_setdependency(cg3_cohort *cohort, uint32_t dep_self, uint32_t dep_parent);
 // The Cohort takes ownership of the Reading here.
-void cg3_cohort_addreading(cg3_cohort_t *cohort, cg3_reading_t *reading);
-size_t cg3_cohort_numreadings(cg3_cohort_t *cohort);
-cg3_reading_t *cg3_cohort_getreading(cg3_cohort_t *cohort, size_t which);
+void cg3_cohort_addreading(cg3_cohort *cohort, cg3_reading *reading);
+size_t cg3_cohort_numreadings(cg3_cohort *cohort);
+cg3_reading *cg3_cohort_getreading(cg3_cohort *cohort, size_t which);
 // This is usually not to be used. The Sentence will take ownership of the Cohort and free it on destruction
-void cg3_cohort_free(cg3_cohort_t *cohort);
+void cg3_cohort_free(cg3_cohort *cohort);
 
-cg3_reading_t *cg3_reading_create(cg3_cohort_t *cohort);
-cg3_status_t cg3_reading_addtag(cg3_reading_t *reading, cg3_tag_t *tag);
-size_t cg3_reading_numtags(cg3_reading_t *reading);
-cg3_tag_t *cg3_reading_gettag(cg3_reading_t *reading, size_t which);
+cg3_reading *cg3_reading_create(cg3_cohort *cohort);
+cg3_status cg3_reading_addtag(cg3_reading *reading, cg3_tag *tag);
+size_t cg3_reading_numtags(cg3_reading *reading);
+cg3_tag *cg3_reading_gettag(cg3_reading *reading, size_t which);
 // This is usually not to be used. The Cohort will take ownership of the Reading and free it on destruction
-void cg3_reading_free(cg3_reading_t *reading);
+void cg3_reading_free(cg3_reading *reading);
 
 #ifdef U_ICU_VERSION_MAJOR_NUM
-cg3_tag_t *cg3_tag_create_u(cg3_applicator_t *applicator, const UChar *text);
+cg3_tag *cg3_tag_create_u(cg3_applicator *applicator, const UChar *text);
 #endif
-cg3_tag_t *cg3_tag_create_u8(cg3_applicator_t *applicator, const char *text);
-cg3_tag_t *cg3_tag_create_u16(cg3_applicator_t *applicator, const uint16_t *text);
-cg3_tag_t *cg3_tag_create_u32(cg3_applicator_t *applicator, const uint32_t *text);
-cg3_tag_t *cg3_tag_create_w(cg3_applicator_t *applicator, const wchar_t *text);
+cg3_tag *cg3_tag_create_u8(cg3_applicator *applicator, const char *text);
+cg3_tag *cg3_tag_create_u16(cg3_applicator *applicator, const uint16_t *text);
+cg3_tag *cg3_tag_create_u32(cg3_applicator *applicator, const uint32_t *text);
+cg3_tag *cg3_tag_create_w(cg3_applicator *applicator, const wchar_t *text);
 
 #ifdef U_ICU_VERSION_MAJOR_NUM
-const UChar *cg3_tag_gettext_u(cg3_tag_t *tag);
+const UChar *cg3_tag_gettext_u(cg3_tag *tag);
 #endif
-const char *cg3_tag_gettext_u8(cg3_tag_t *tag);
-const uint16_t *cg3_tag_gettext_u16(cg3_tag_t *tag);
-const uint32_t *cg3_tag_gettext_u32(cg3_tag_t *tag);
-const wchar_t *cg3_tag_gettext_w(cg3_tag_t *tag);
+const char *cg3_tag_gettext_u8(cg3_tag *tag);
+const uint16_t *cg3_tag_gettext_u16(cg3_tag *tag);
+const uint32_t *cg3_tag_gettext_u32(cg3_tag *tag);
+const wchar_t *cg3_tag_gettext_w(cg3_tag *tag);
 
 #ifdef __cplusplus
 }
