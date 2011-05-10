c8ec5df27b7cbef35e17ec5ee4c2e3fb282a6afb
Tino Didriksen <mail@tinodidriksen.com>
2011-05-10 10:22:08 +0000

    CMake tuning
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@7134 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,5 @@
 echo "- rm -rf CMake caches" && \
 rm -rf CMakeCache.txt cmake_install.cmake CMakeFiles src/CMakeFiles src/cmake_install.cmake && \
-echo "- cmake ." && \
-cmake . && \
-echo "You may now perform: make -j3"
+echo "- cmake Release ." && \
+cmake -DCMAKE_BUILD_TYPE=Release . && \
+echo "- You may now perform: make -j3"
