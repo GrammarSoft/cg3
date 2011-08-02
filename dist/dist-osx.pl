6973762e7fcc6405394c43080f48a739c879fc9d
Tino Didriksen <mail@tinodidriksen.com>
2011-08-02 12:09:34 +0000

    Dist OS X
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@7408 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- dist/dist-osx.pl
+++ dist/dist-osx.pl
@@ -2,20 +2,24 @@
 use strict;
 use warnings;
 
+my $dname = `dirname "$0"`;
+chdir($dname);
+
 print `svn up --ignore-externals`;
 my $revision = `svnversion -n`;
 $revision =~ s/^([0-9]+).*/$1/g;
 
-chdir('/tmp');
-`rm -rfv 'vislcg3-0.9.7.$revision-osx' 2>&1`;
-mkdir('vislcg3-0.9.7.'.$revision.'-osx');
-chdir('vislcg3-0.9.7.'.$revision.'-osx');
+print `rm -rfv '/tmp/vislcg3-0.9.7.$revision-osx*' 2>&1`;
+mkdir('/tmp/vislcg3-0.9.7.'.$revision.'-osx');
+chdir('/tmp/vislcg3-0.9.7.'.$revision.'-osx');
 mkdir('lib');
 mkdir('bin');
+print `cp -av "$dname/osx/*" ./ 2>&1`;
 print `cp -av /usr/local/bin/vislcg3 ./bin/ 2>&1`;
 print `cp -av /usr/local/bin/cg-* ./bin/ 2>&1`;
 print `cp -av /usr/local/bin/cg3* ./bin/ 2>&1`;
 print `cp -av /usr/local/lib/libicu*.dylib ./lib/ 2>&1`;
 
 chdir('/tmp');
-`tar -zcvf 'vislcg3-0.9.7.$revision-osx.tar.gz' 'vislcg3-0.9.7.$revision-osx' 2>&1`;
+print `tar -zcvf 'vislcg3-0.9.7.$revision-osx.tar.gz' 'vislcg3-0.9.7.$revision-osx' 2>&1`;
+print `ls -l '/tmp/vislcg3-0.9.7.$revision-osx.tar.gz'`;
