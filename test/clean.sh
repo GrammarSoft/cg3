9f5bde85ab64c1f3d31c0a3ae4b31fbf32197c26
Tino Didriksen <mail@tinodidriksen.com>
2011-11-04 14:52:21 +0000

    Changed .bin3 to .cg3b everywhere.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@7641 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- test/clean.sh
+++ test/clean.sh
@@ -4,4 +4,4 @@ rm -f T*/output*.txt
 rm -f T*/stdout*.txt
 rm -f T*/stderr*.txt
 rm -f T*/grammar.out.txt
-rm -f T*/grammar.bin3
+rm -f T*/grammar.cg3b
--- vapply.sh
+++ vapply.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #svn up
 make -j3
-./src/vislcg3 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.bin3
+./src/vislcg3 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
 mkdir -p vapply
 cd vapply
 rm -fv callgrind.*
 rm -fv annotated
-g++ -DHAVE_BOOST -I~/tmp/boost_1_46_1 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
-head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -v -C UTF-8 -g ../dancg.bin3 > output.txt
-#head -n 2000 ../comparison/arboretum_stripped.txt | valgrind ./vislcg3.debug -v -C UTF-8 -g ../dancg.bin3 > output.txt
+g++ -DHAVE_BOOST -I~/tmp/boost_1_47_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
+head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -v -C UTF-8 -g ../dancg.cg3b > output.txt
+#head -n 2000 ../comparison/arboretum_stripped.txt | valgrind ./vislcg3.debug -v -C UTF-8 -g ../dancg.cg3b > output.txt
 callgrind_annotate --tree=both --auto=yes > annotated
