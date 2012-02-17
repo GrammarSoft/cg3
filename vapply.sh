0ea06e9b67d9ca74e89ddff726912b646b29343f
Tino Didriksen <mail@tinodidriksen.com>
2012-02-17 12:41:04 +0000

    cg3
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@8001 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- vapply.sh
+++ vapply.sh
@@ -6,7 +6,7 @@ mkdir -p vapply
 cd vapply
 rm -fv callgrind.*
 rm -fv annotated
-g++ -DHAVE_BOOST -I~/tmp/boost_1_47_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
+g++ -DHAVE_BOOST -I~/tmp/boost_1_48_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
 head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -v -C UTF-8 -g ../dancg.cg3b > output.txt
 #head -n 2000 ../comparison/arboretum_stripped.txt | valgrind ./vislcg3.debug -v -C UTF-8 -g ../dancg.cg3b > output.txt
 callgrind_annotate --tree=both --auto=yes > annotated
