2b36d33d60462c4a450b8dfae681e1b1792f46ed
Tino Didriksen <mail@tinodidriksen.com>
2012-08-22 18:39:42 +0000

    Boost 1.51.0
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@8467 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- get-boost.sh
+++ get-boost.sh
@@ -2,10 +2,10 @@
 cd "`dirname "$0"`"
 rm -rfv boost*
 if test -x "/usr/bin/wget"; then
-	wget http://sourceforge.net/projects/boost/files/boost/1.50.0/boost_1_50_0.tar.bz2/download -O boost_1_50_0.tar.bz2
+	wget http://sourceforge.net/projects/boost/files/boost/1.51.0/boost_1_51_0.tar.bz2/download -O boost_1_51_0.tar.bz2
 elif test -x "/usr/bin/curl"; then
-	curl -L --max-redirs 10 http://sourceforge.net/projects/boost/files/boost/1.50.0/boost_1_50_0.tar.bz2/download > boost_1_50_0.tar.bz2
+	curl -L --max-redirs 10 http://sourceforge.net/projects/boost/files/boost/1.51.0/boost_1_51_0.tar.bz2/download > boost_1_51_0.tar.bz2
 fi
-tar -jxvf boost_1_50_0.tar.bz2 boost_1_50_0/boost
-cp -avf boost_1_50_0/boost include/
+tar -jxvf boost_1_51_0.tar.bz2 boost_1_51_0/boost
+cp -avf boost_1_51_0/boost include/
 rm -rfv boost*
