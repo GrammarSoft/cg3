cde954b81ed094019a5ceb52a0cfc62121195176
Tino Didriksen <mail@tinodidriksen.com>
2013-05-24 10:12:24 +0000

    Cleanup
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9009 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- autogen.sh
+++
@@ -1,1 +0,0 @@
-echo "Error: This script is deprecated. Use ./cmake.sh instead, possibly after installing CMake from apt-get, yum, or http://cmake.org/ . Also note that we now require Boost, preferably a recent version from http://boost.org/ or via ./get-boost.sh"
