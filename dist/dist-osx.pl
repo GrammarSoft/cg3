e19843539bf02e096dd6a99629e1c75c917fc436
Tino Didriksen <mail@tinodidriksen.com>
2013-06-17 12:12:41 +0000

    ICU rewrite version bump
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9046 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- dist/dist-osx.pl
+++ dist/dist-osx.pl
@@ -22,9 +22,9 @@ print STDERR `cp -av $dname/../src/cg-proc ./bin/ 2>&1`;
 print STDERR `cp -av $dname/../src/cg-conv ./bin/ 2>&1`;
 foreach my $bin (('vislcg3','cg-comp','cg-proc','cg-conv')) {
    print STDERR `ln -sv ./bin/$bin $bin`;
-   print STDERR `install_name_tool -change libicuuc.48.dylib \@executable_path/../lib/libicuuc.48.dylib ./bin/$bin`;
-   print STDERR `install_name_tool -change libicuio.48.dylib \@executable_path/../lib/libicuio.48.dylib ./bin/$bin`;
-   print STDERR `install_name_tool -change libicui18n.48.dylib \@executable_path/../lib/libicui18n.48.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicuuc.51.dylib \@executable_path/../lib/libicuuc.51.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicuio.51.dylib \@executable_path/../lib/libicuio.51.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicui18n.51.dylib \@executable_path/../lib/libicui18n.51.dylib ./bin/$bin`;
 }
 print STDERR `cp -av $dname/../scripts/cg3-autobin.pl ./bin/ 2>&1`;
 print STDERR `cp -av /usr/local/lib/libicu*.dylib ./lib/ 2>&1`;
