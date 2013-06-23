6cea12f12a11a5d078ed5d5c1b0347d73cfb16b3
Tino Didriksen <mail@tinodidriksen.com>
2013-06-23 08:16:11 +0000

    Re-enable duplicate tag elimination (which was causing the difference) and useless barrier elimination (which was innocent after all).
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9072 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/GrammarApplicator_reflow.cpp
+++ src/GrammarApplicator_reflow.cpp
@@ -451,9 +451,11 @@ uint32_t GrammarApplicator::addTagToReading(Reading& reading, uint32_t utag, boo
 	if (it != grammar->sets_by_tag.end()) {
 		reading.parent->possible_sets.insert(it->second.begin(), it->second.end());
 	}
-	reading.tags.insert(utag);
-	reading.tags_list.push_back(utag);
-	reading.tags_bloom.insert(utag);
+	if (ordered || reading.tags.find(utag) == reading.tags.end()) {
+		reading.tags.insert(utag);
+		reading.tags_list.push_back(utag);
+		reading.tags_bloom.insert(utag);
+	}
 	if (grammar->parentheses.find(utag) != grammar->parentheses.end()) {
 		reading.parent->is_pleft = utag;
 	}
