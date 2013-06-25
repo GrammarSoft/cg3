583ca30d72937a1415c92f920c7bdf844f3a8db1
Tino Didriksen <mail@tinodidriksen.com>
2013-06-25 15:37:47 +0000

    Naive fix
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9087 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- make-naive.sh
+++ make-naive.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc ./src/all_vislcg3.cpp -o vislcg3
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc ./src/all_cg-comp.cpp -o cg-comp
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc ./src/all_cg-proc.cpp -o cg-proc
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc ./src/all_cg-conv.cpp -o cg-conv
+echo "Building vislcg3 ..."
+g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_vislcg3.cpp -o vislcg3
+echo "Building cg-comp ..."
+g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_comp.cpp -o cg-comp
+echo "Building cg-proc ..."
+g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_proc.cpp -o cg-proc
+echo "Building cg-conv ..."
+g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_conv.cpp -o cg-conv
