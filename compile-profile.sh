51bfed8b8f4a1191fb8065d745ab4bf9464ad2c2
Tino Didriksen <mail@tinodidriksen.com>
2013-09-25 11:54:46 +0000

    Enabling C++11 makes grammar parsing 2.08% faster and rule applying 0.22% slower.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9245 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- compile-profile.sh
+++ compile-profile.sh
@@ -5,10 +5,10 @@ rm -fv gcov/*.gcda
 rm -fv gcov/*.gcno
 rm -fv gcov/vislcg3
 cd gcov
-g++ -DHAVE_BOOST -I~/tmp/boost_1_49_0 -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3
+g++ -std=c++11 -DHAVE_BOOST -I~/tmp/boost_1_54_0 -pipe -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -fno-rtti -fprofile-arcs -ftest-coverage -ffor-scope -L/usr/local/lib64 -L/usr/local/lib -ltcmalloc -licuio -licuuc -I../include -I../include/exec-stream ../src/all_vislcg3.cpp -o vislcg3
 cd ..
 mkdir -p gprof
 rm -fv gprof/vislcg3
 rm -fv gprof/gmon.out
 cd gprof
-g++ -DHAVE_BOOST -I~/tmp/boost_1_49_0 -pipe -pg -Wall -Wextra -Wno-deprecated -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3
+g++ -std=c++11 -DHAVE_BOOST -I~/tmp/boost_1_54_0 -pipe -pg -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib64 -L/usr/local/lib -ltcmalloc -licuio -licuuc -I../include -I../include/exec-stream ../src/all_vislcg3.cpp -o vislcg3
