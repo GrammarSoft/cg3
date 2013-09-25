c48c05b104e2f212b399c90b18084a01409361ed
Tino Didriksen <mail@tinodidriksen.com>
2013-09-25 15:50:56 +0000

    Scripts
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9247 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- profile-apply.sh
+++ profile-apply.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 98 or 11
+export CXXV=11
 mkdir -p gprof
 mkdir -p gcov
 
@@ -8,12 +10,12 @@ make -j5
 ./compile-profile.sh
 
 cd gcov/
-#head -n 3000 arboretum_stripped.txt | ../src/vislcg3 -v -C UTF-8 -g ../dancg.cg3b > out1.txt
-head -n 3000 arboretum_stripped.txt | ./vislcg3 -v -C UTF-8 -g ../dancg.cg3b > out2.txt
+#head -n 3000 arboretum_stripped.txt | ../src/vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out1.txt
+head -n 3000 arboretum_stripped.txt | ./vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out2.txt
 find ../src/ -type f -name '*.cpp' -exec gcov -o . {} \; >/dev/null 2>/dev/null
 gcov -o . ../src/GrammarApplicator_runRules.cpp  >/dev/null 2>/dev/null
 gcov -o . ../src/GrammarApplicator_matchSet.cpp  >/dev/null 2>/dev/null
 
 cd ../gprof/
-head -n 3000 arboretum_stripped.txt | ./vislcg3 -v -C UTF-8 -g ../dancg.cg3b > out3.txt
+head -n 3000 arboretum_stripped.txt | ./vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out3.txt
 gprof vislcg3 >flat.txt
--- profile-parse.sh
+++ profile-parse.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 98 or 11
+export CXXV=11
 mkdir -p gprof
 mkdir -p gcov
 
@@ -6,10 +8,10 @@ svn up
 ./compile-profile.sh
 
 cd gcov/
-./vislcg3 -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
+./vislcg3-c++$CXXV -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
 gcov -o . ../src/*.cpp >/dev/null 2>/dev/null
 gcov -o . ../src/TextualParser.cpp  >/dev/null 2>/dev/null
 
 cd ../gprof/
-./vislcg3 -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
+./vislcg3-c++$CXXV -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
 gprof vislcg3 >flat.txt
