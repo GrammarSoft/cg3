cab00c87c972604a5d0005002b6b6bd1c2624864
Tino Didriksen <mail@tinodidriksen.com>
2013-09-25 11:54:46 +0000

    Enabling C++11 makes grammar parsing 2.08% faster and rule applying 0.22% slower.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9245 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,5 @@
 echo "- rm -rf CMake caches" && \
-rm -rf install_manifest.txt CMakeCache.txt cmake_install.cmake CMakeFiles src/CMakeFiles src/cmake_install.cmake && \
-echo "- cmake Release ." && \
-cmake -DCMAKE_BUILD_TYPE=Release $@ . && \
+rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles src/CMakeFiles src/*.cmake _CPack_Packages && \
+echo "- cmake" "$@" "." && \
+cmake "$@" . && \
 echo "- You may now perform: make -j3"
--- vparse.sh
+++ vparse.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# 98 or 11
+CXXV=11
 #svn up
 mkdir -p vparse
 cd vparse
 rm -f callgrind.*
-rm -f annotated
-g++ -DHAVE_BOOST -I~/tmp/boost_1_49_0 -Wall -Wextra -Wno-deprecated -pipe -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib -ltcmalloc -licuio -licuuc ../src/all_vislcg3.cpp -o vislcg3.debug
-valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -C UTF-8 --grammar-only -g ~/parsers/dansk/etc/dancg
+mv -vf annotated annotated.old
+g++ -std=c++$CXXV -DHAVE_BOOST -I~/tmp/boost_1_54_0 -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -pipe -O3 -g3 -fno-rtti -ffor-scope -L/usr/local/lib64 -L/usr/local/lib -licuio -licuuc -I../include -I../include/exec-stream ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV.debug
+valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3-c++$CXXV.debug -C UTF-8 --grammar-only -g ~/parsers/dansk/etc/dancg
 callgrind_annotate --tree=both --auto=yes > annotated
