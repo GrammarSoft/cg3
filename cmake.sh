159f89e722043dad43fe14b569e7f9e28b355a16
Tino Didriksen <mail@tinodidriksen.com>
2014-04-10 13:09:50 +0000

    cmake.sh now accepts --prefix X and --prefix=X
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9853 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,28 @@
-echo "- rm -rf CMake caches" && \
-rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles src/CMakeFiles src/*.cmake _CPack_Packages && \
-echo "- cmake" "$@" "." && \
-cmake "$@" . && \
+#!/bin/bash -e
+args=()
+
+while [[ $# > 0 ]];
+do
+	case "$1" in
+	--prefix)
+		args+=("-DCMAKE_INSTALL_PREFIX=$2")
+		shift 2
+		;;
+	--prefix=*)
+		args+=("-DCMAKE_INSTALL_PREFIX=${1#*=}")
+		shift
+		;;
+	*)
+		args+=("$1")
+		shift
+		;;
+	esac
+done
+
+set -- "${args[@]}"
+
+echo "- rm -rf CMake caches"
+rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles src/CMakeFiles src/*.cmake _CPack_Packages
+echo "- cmake " "$@" "."
+cmake "$@" .
 echo "- You may now perform: make -j3"
