dd9cf30a0aa7ff9fa69d48d4a826f372ef616414
Tino Didriksen <mail@tinodidriksen.com>
2014-05-08 09:07:04 +0000

    Document that ldconfig is needed
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9894 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cmake.sh
+++ cmake.sh
@@ -22,7 +22,7 @@ done
 set -- "${args[@]}"
 
 echo "- rm -rf CMake caches"
-rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles src/CMakeFiles src/*.cmake _CPack_Packages
+rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles src/CMakeFiles src/*.cmake _CPack_Packages Testing test/T_*/grammar.cg3b
 echo "- cmake " "$@" "."
 cmake "$@" .
 echo "- You may now perform: make -j3"
