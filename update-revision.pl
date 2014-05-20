3c372f6d0552f302587da3af7d26b8e106d95a8d
Tino Didriksen <mail@tinodidriksen.com>
2014-05-20 18:41:08 +0000

    Set cg3-autobin.pl revision during build instead of manually
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@9927 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- update-revision.pl
+++ update-revision.pl
@@ -8,7 +8,6 @@ $revision =~ s/^([0-9]+).*/$1/g;
 $revision += 1;
 
 `/usr/bin/perl -e 's/CG3_REVISION = [0-9]+;\$/CG3_REVISION = $revision;/;' -pi src/version.hpp`;
-`/usr/bin/perl -e 's/\$revision = [0-9]+;\$/\$revision = $revision;/;' -pi scripts/cg3-autobin.pl`;
 `svn2cl --group-by-day -i --reparagraph --limit 350`;
 
 print "Set revision to $revision.\n";
