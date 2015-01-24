e99e73ed94bdc5e7b028bf3ed202301e767d5ddb
Tino Didriksen <mail@tinodidriksen.com>
2015-01-24 12:37:48 +0000

    Bump copyright to 2015; Fix profiling scripts
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10377 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- vparse.sh
+++ vparse.sh
@@ -6,6 +6,6 @@ mkdir -p vparse
 cd vparse
 rm -f callgrind.*
 mv -vf annotated annotated.old
-g++ -std=c++$CXXV -DNDEBUG -Wall -Wextra -Wno-deprecated -pipe -O3 -g3 -L/usr/local/lib64 -L/usr/local/lib -licuio -licuuc -I../include -I../include/exec-stream ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV.debug
+g++ -std=c++$CXXV -DNDEBUG -Wall -Wextra -Wno-deprecated -pthread -pipe -O3 -g3 -I../include -I../include/exec-stream ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV.debug -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc
 valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3-c++$CXXV.debug -C UTF-8 --grammar-only -g ~/parsers/dansk/etc/dancg
 callgrind_annotate --tree=both --auto=yes > annotated
