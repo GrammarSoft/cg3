a1162ce63c4d933160b03e88bc842efcd0239cf0
Tino Didriksen <mail@tinodidriksen.com>
2015-01-28 19:30:40 +0000

    ...and now with all the code that got deleted.
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10391 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- test/runall.pl
+++ test/runall.pl
@@ -21,8 +21,45 @@ my @binlist = (
 	"../Release/vislcg3",
 	"../src/vislcg3",
 	"../vislcg3",
-);
+	);
+my @unlinks = (
+   'diff.txt',
+   'output.txt',
+   'grammar.cg3b',
+   'diff.bin.txt',
+   'output.bin.txt',
+   );
 my $binary = "vislcg3";
+
+sub run_pl {
+   my ($binary,$override,$args) = @_;
+
+   `"$binary" $args $override -g grammar.cg3 -I input.txt -O output.txt >stdout.txt 2>stderr.txt`;
+   `diff -B expected.txt output.txt >diff.txt`;
+
+   if (-s "diff.txt") {
+      print STDERR "Fail ";
+   } else {
+      print STDERR "Success ";
+   }
+
+   `"$binary" $args $override -g grammar.cg3 --grammar-only --grammar-bin grammar.cg3b >stdout.bin.txt 2>stderr.bin.txt`;
+   my $gf = undef;
+   for my $g (glob('*.cg3b*')) {
+      `"$binary" $args $override -g '$g' -I input.txt -O output.bin.txt >>stdout.bin.txt 2>>stderr.bin.txt`;
+      `diff -B expected.txt output.bin.txt >diff.bin.txt`;
+      if (-s "diff.bin.txt") {
+         $gf = $g;
+         last;
+      }
+   }
+
+   if (-s "diff.bin.txt") {
+      print STDERR "Fail ($gf).\n";
+   } else {
+      print STDERR "Success.\n";
+   }
+}
 
 foreach (@binlist) {
 	if (-x $_) {
@@ -47,13 +84,14 @@ print STDERR "Binary found at: $binary\n";
 
 print STDERR "\nRunning tests...\n";
 
-my @tests = grep { -x } glob('./T*/run.pl');
+my @tests = grep { -x } glob('./T_*');
 foreach (@tests) {
 	if ($ARGV[0] && $ARGV[0] ne "" && !(/$ARGV[0]/i)) {
 		next;
 	}
 	chdir $bindir or die("Error: Could not change directory to $bindir !");
 	my ($test) = m/^.*?(T[^\/]+).*$/;
+	chdir $test or die("Error: Could not change directory to $test !");
 	print STDERR "$test: ";
 	for (my $i=length $test;$i<30;$i++) {
 		print STDERR " ";
@@ -61,26 +99,26 @@ foreach (@tests) {
 	if (-s "./$test/byline.txt") {
 		print STDERR "(".`cat "./$test/byline.txt"`.") ";
 	}
-	if (-e "./".$test."/diff.txt") {
-	    unlink "./".$test."/diff.txt";
+	for my $u (@unlinks) {
+	   if (-e $u) {
+	      unlink $u;
+	   }
 	}
-	if (-e "./".$test."/output.txt") {
-	    unlink "./".$test."/output.txt";
-	}
-	if (-e "./".$test."/grammar.cg3b") {
-	    unlink "./".$test."/grammar.cg3b";
-	}
-	if (-e "./".$test."/diff.bin.txt") {
-	    unlink "./".$test."/diff.bin.txt";
-	}
-	if (-e "./".$test."/output.bin.txt") {
-	    unlink "./".$test."/output.bin.txt";
-	}
+
 	my $c = '""';
 	if ($ARGV[1] && $ARGV[1] ne "") {
 		$c = '"'.$ARGV[1].'"';
 	}
-	`$_ "$binary" \Q$c\E`;
+	my $args = '';
+	if (-s 'args.txt') {
+	   $args = `cat args.txt`;
+	}
+	if (-x 'run.pl') {
+	   `./run.pl "$binary" \Q$c\E $args`;
+	}
+	else {
+	   run_pl($binary, $c, $args);
+	}
 }
 
 print STDERR "\n";
