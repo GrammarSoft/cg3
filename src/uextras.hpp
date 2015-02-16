fed6b5f9c88aa699cb0d42c7a042186ddeb5ddde
Tino Didriksen <mail@tinodidriksen.com>
2015-02-16 15:37:10 +0000

    Print basename of grammar before most parsing diagnostics
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10501 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/uextras.hpp
+++ src/uextras.hpp
@@ -26,6 +26,29 @@
 #include "stdafx.hpp"
 #include "Strings.hpp"
 
+#ifdef _MSC_VER
+inline const char *basename(const char *path) {
+	if (path != NULL) {
+		// Find the last position of \ or / in the path name
+		const char *pos = std::max(strrchr(path, '\\'), strrchr(path, '/'));
+
+		if (pos != NULL) { // If a \ char was found...
+			if (pos + 1 != NULL) // If it is not the last character in the string...
+				return pos + 1; // then return a pointer to the first character after \.
+			else
+				return pos; // else return a pointer to \.
+		}
+		else { // If a \ char was NOT found
+			return path; // return the pointer passed to basename (this is probably non-conformant)
+		}
+
+	}
+	else { // If path == NULL, return "."
+		return ".";
+	}
+}
+#endif
+
 namespace CG3 {
 
 inline int ux_isSetOp(const UChar *it) {
