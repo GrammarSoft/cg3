395e9c815f7ea96940441553ac19b85715198808
Tino Didriksen <mail@tinodidriksen.com>
2015-07-01 09:01:53 +0000

    Unused
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10825 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- make-naive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Building vislcg3 ..."
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_vislcg3.cpp -o vislcg3
-echo "Building cg-comp ..."
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_comp.cpp -o cg-comp
-echo "Building cg-proc ..."
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_proc.cpp -o cg-proc
-echo "Building cg-conv ..."
-g++ -DHAVE_BOOST -pipe -Wall -Wextra -Wno-deprecated -O3 -fno-rtti -ffor-scope -licuio -licuuc $@ ./src/all_cg_conv.cpp -o cg-conv
