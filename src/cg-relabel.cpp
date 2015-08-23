7a339721a52f4c42267fd2b6c0d38f51ba5e2b6d
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-08-23 17:42:13 +0000

    cg-relabel command using binary grammars
    New relabel format (ab)using vislcg3 format:
      MAP (N)	 (n) OR (np) ;
    means relabel the tag N into n or np, alternatively:
      LIST N = n np;
      MAP (N) N;
    
    The from-part is only ever one tag, but the to-part can be several.
    Full-on SET's as to-parts still TODO, but should be doable.
    
    No longer uses GrammarWriter at all.
    
    Some cleanup TODO.
    
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10897 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ src/cg-relabel.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2007-2015, GrammarSoft ApS
+ * Developed by Tino Didriksen <mail@tinodidriksen.com>
+ * Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
+ *
+ * This file is part of VISL CG-3
+ *
+ * VISL CG-3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VISL CG-3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VISL CG-3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.hpp"
+
+#include "Grammar.hpp"
+#include "GrammarWriter.hpp"
+#include "BinaryGrammar.hpp"
+#include "TextualParser.hpp"
+#include "Relabeller.hpp"
+
+#ifndef _WIN32
+#include <libgen.h>
+#endif
+
+#include "version.hpp"
+
+using CG3::CG3Quit;
+
+void endProgram(char *name) {
+        if (name != NULL) {
+                fprintf(stdout, "VISL CG-3 Relabeller version %u.%u.%u.%u\n",
+                        CG3_VERSION_MAJOR, CG3_VERSION_MINOR, CG3_VERSION_PATCH, CG3_REVISION);
+                std::cout << basename(name) << ": relabel a binary grammar using a relabelling file" << std::endl;
+                std::cout << "USAGE: " << basename(name) << " input_grammar_file relabel_rule_file output_grammar_file" << std::endl;
+        }
+        exit(EXIT_FAILURE);
+}
+
+
+
+
+
+// like libcg3's, but with a non-void grammar …
+CG3::Grammar *cg3_grammar_load(const char *filename, UFILE* ux_stdout, UFILE* ux_stderr) {
+	using namespace CG3;
+	std::ifstream input(filename, std::ios::binary);
+	if (!input) {
+		u_fprintf(ux_stderr, "CG3 Error: Error opening %s for reading!\n", filename);
+		return 0;
+	}
+	if (!input.read(&cbuffers[0][0], 4)) {
+		u_fprintf(ux_stderr, "CG3 Error: Error reading first 4 bytes from grammar!\n");
+		return 0;
+	}
+	input.close();
+
+	Grammar *grammar = new Grammar;
+	grammar->ux_stderr = ux_stderr;
+	grammar->ux_stdout = ux_stdout;
+
+	std::auto_ptr<IGrammarParser> parser;
+
+	if (cbuffers[0][0] == 'C' && cbuffers[0][1] == 'G' && cbuffers[0][2] == '3' && cbuffers[0][3] == 'B') {
+		u_fprintf(ux_stderr, "CG3 Info: Binary grammar detected.\n");
+		parser.reset(new BinaryGrammar(*grammar, ux_stderr));
+	}
+	else {
+		parser.reset(new TextualParser(*grammar, ux_stderr));
+	}
+	if (parser->parse_grammar_from_file(filename, uloc_getDefault(), ucnv_getDefaultName())) {
+		u_fprintf(ux_stderr, "CG3 Error: Grammar could not be parsed!\n");
+		return 0;
+	}
+
+	grammar->reindex();
+
+	return grammar;
+}
+
+int main(int argc, char *argv[]) {
+        UFILE *ux_stdout = 0;
+        UFILE *ux_stderr = 0;
+        UErrorCode status = U_ZERO_ERROR;
+
+        if (argc != 4) {
+                endProgram(argv[0]);
+        }
+
+        /* Initialize ICU */
+        u_init(&status);
+        if (U_FAILURE(status) && status != U_FILE_ACCESS_ERROR) {
+                std::cerr << "Error: Cannot initialize ICU. Status = " << u_errorName(status) << std::endl;
+                CG3Quit(1);
+        }
+        status = U_ZERO_ERROR;
+
+        ucnv_setDefaultName("UTF-8");
+        const char *codepage_default = ucnv_getDefaultName();
+        uloc_setDefault("en_US_POSIX", &status);
+        const char *locale_default = uloc_getDefault();
+
+        ux_stdout = u_finit(stdout, locale_default, codepage_default);
+        ux_stderr = u_finit(stderr, locale_default, codepage_default);
+
+	CG3::Grammar* grammar = cg3_grammar_load(argv[1], ux_stdout, ux_stderr);
+	CG3::Grammar* relabel_grammar = cg3_grammar_load(argv[2], ux_stdout, ux_stderr);
+
+	CG3::Relabeller relabeller(*grammar, *relabel_grammar, ux_stderr);
+	relabeller.relabel();
+
+        FILE *gout = fopen(argv[3], "wb");
+        if (gout) {
+		CG3::BinaryGrammar writer(*grammar, ux_stderr);
+		writer.writeBinaryGrammar(gout);
+        }
+        else {
+                std::cerr << "Could not write grammar to " << argv[3] << std::endl;
+        }
+
+	delete relabel_grammar;
+	relabel_grammar = 0;
+	delete grammar;
+	grammar = 0;
+
+        u_fclose(ux_stderr);
+        u_fclose(ux_stdout);
+
+        u_cleanup();
+
+        return status;
+}
