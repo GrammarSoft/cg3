04c4373004458fcbd45365f073596b5eec96f3a3
Tino Didriksen <mail@tinodidriksen.com>
2015-09-23 08:25:01 +0000

    Documented SplitCohort
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@10982 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- todo.sh
+++ todo.sh
@@ -2,4 +2,4 @@
 rm -f TODO.list
 cat TODO > TODO.list
 echo "----------" >> TODO.list
-grep -i todo src/* >> TODO.list
+grep -i todo src/* | perl -wpne 's/^([^:]+):\s*(.+)$/$2\t: $1/;' | perl -wpne 's@^//\s*@@g;' | LC_ALL=C sort >> TODO.list
