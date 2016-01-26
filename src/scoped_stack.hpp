e4f658b09f0c62b027fe660efec8e8462d8a10df
Tino Didriksen <mail@tinodidriksen.com>
2016-01-26 16:16:30 +0000

    Copyright and rev bump
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@11264 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/scoped_stack.hpp
+++ src/scoped_stack.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2007-2015, GrammarSoft ApS
+* Copyright (C) 2007-2016, GrammarSoft ApS
 * Developed by Tino Didriksen <mail@tinodidriksen.com>
 * Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
 *
