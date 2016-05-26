dc11b21d4a1ded16d886321ea0ea318eb5bede5a
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-05-26 14:39:31 +0000

    cg-mwesplit: new binary for splitting multiwords
    like https://github.com/unhammer/cg-mwesplit but rewritten to inherit
    GrammarApplicator and use the standard vislcg3 parsing.
    
    Takes no options, just stdin and stdout:
    
    $ cg-mwesplit < infile > outfile
    
    More typically, it’ll be in a pipeline after hfst-tokenise and some step
    that disambiguates multiwords using vislcg3:
    
    $ echo words go here | hfst-tokenise --gtd tokeniser.pmhfst | vislcg3 -g mwe-dis.cg3 | cg-mwesplit
    
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@11623 cb2587b3-b7ff-0310-8c81-a2a651690ada

---
+++ src/MweSplitApplicator.cpp
@@ -0,0 +1,175 @@
+/*
+* Copyright (C) 2007-2016, GrammarSoft ApS
+* Developed by Tino Didriksen <mail@tinodidriksen.com>
+* Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
+*
+* This file is part of VISL CG-3
+*
+* VISL CG-3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* VISL CG-3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with VISL CG-3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "MweSplitApplicator.hpp"
+
+namespace CG3 {
+
+MweSplitApplicator::MweSplitApplicator(UFILE *ux_err)
+	: GrammarApplicator(ux_err)
+{
+}
+
+
+void MweSplitApplicator::runGrammarOnText(istream& input, UFILE *output) {
+	GrammarApplicator::runGrammarOnText(input, output);
+}
+
+
+const Tag* MweSplitApplicator::maybeWfTag(const Reading* r) {
+	foreach (tter, r->tags_list) {
+		if ((!show_end_tags && *tter == endtag) || *tter == begintag) {
+			continue;
+		}
+		if (*tter == r->baseform || *tter == r->parent->wordform->hash) {
+			continue;
+		}
+		const Tag *tag = single_tags[*tter];
+		// If we are to split, there has to be at least one wordform on a head (not-sub) reading
+		if (tag->type & T_WORDFORM) {
+			return tag;
+		}
+	}
+	return NULL;
+
+}
+
+std::vector<Cohort*> MweSplitApplicator::splitMwe(Cohort* cohort) {
+	const UChar rtrimblank[] = { ' ', '\n', '\r', '\t', 0 };
+	const UChar textprefix[] = { ':', 0 };
+	std::vector<Cohort*> cos;
+	size_t n_wftags = 0;
+	size_t n_goodreadings = 0;
+	foreach (rter1, cohort->readings) {
+		if(maybeWfTag(*rter1) != NULL) {
+			++n_wftags;
+		}
+		++n_goodreadings;
+	}
+
+	if(n_wftags < n_goodreadings) {
+		if(n_wftags > 0) {
+			u_fprintf(ux_stderr, "WARNING: Line %u: Some but not all main-readings of %S had wordform-tags (not completely mwe-disambiguated?), not splitting.\n", numLines, cohort->wordform->tag.c_str());
+			// We also don't split if wordform-tags were only on sub-readings, but should we warn on such faulty input?
+		}
+		cos.push_back(cohort);
+		return cos;
+	}
+	foreach(r, cohort->readings) {
+		size_t pos = -1;
+		Reading *prev = NULL;	// prev == NULL || prev->next == rNew (or a ->next of rNew)
+		for(Reading *sub = (*r); sub; sub = sub->next) {
+			const Tag* wfTag = maybeWfTag(sub);
+			if(wfTag == NULL) {
+				prev = prev->next;
+			}
+			else {
+				++pos;
+				Cohort* c;
+				while(cos.size() < pos+1) {
+					c = alloc_cohort(cohort->parent);
+					c->global_number = gWindow->cohort_counter++;
+					cohort->parent->appendCohort(c);
+					cos.push_back(c);
+				}
+				c = cos[pos];
+
+				const size_t wfEnd = wfTag->tag.size()-3; // index before the final '>"'
+				const size_t i = 1 + wfTag->tag.find_last_not_of(rtrimblank, wfEnd);
+				const UString &wf = wfTag->tag.substr(0, i) + wfTag->tag.substr(wfEnd+1);
+				if(c->wordform != 0 && wf != c->wordform->tag) {
+					u_fprintf(ux_stderr, "WARNING: Line %u: Ambiguous wordform-tags for same cohort, '%S' vs '%S', not splitting.\n", numLines, wf.c_str(), c->wordform->tag.c_str());
+					cos.clear();
+					cos.push_back(cohort);
+					return cos;
+				}
+				c->wordform = addTag(wf);
+				if(i < wfEnd+1) {
+					c->text = textprefix + wfTag->tag.substr(i, wfEnd+1-i);
+				}
+
+				Reading *rNew = alloc_reading(*sub);
+				for (size_t i = 0; i < rNew->tags_list.size(); ++i) {
+					BOOST_AUTO(&tter, rNew->tags_list[i]);
+					if(tter == wfTag->hash || tter == rNew->parent->wordform->hash) {
+						rNew->tags_list.erase(rNew->tags_list.begin() + i);
+						rNew->tags.erase(tter);
+					}
+				}
+				cos[pos]->appendReading(rNew);
+				rNew->parent = cos[pos];
+
+				if(prev != NULL) {
+					free_reading(prev->next);
+					prev->next = 0;
+				}
+				prev = rNew;
+			}
+		}
+	}
+	if(cos.size() == 0) {
+		u_fprintf(ux_stderr, "WARNING: Line %u: Tried splitting %S, but got no new cohorts; shouldn't happen.", numLines, cohort->wordform->tag.c_str());
+		cos.push_back(cohort);
+	}
+	// The last word forms are the top readings:
+	cos[0]->text = cohort->text;
+	std::reverse(cos.begin(), cos.end());
+	return cos;
+}
+
+
+void MweSplitApplicator::printSingleWindow(SingleWindow *window, UFILE *output) {
+	boost_foreach (uint32_t var, window->variables_output) {
+		Tag *key = single_tags[var];
+		BOOST_AUTO(iter, window->variables_set.find(var));
+		if (iter != window->variables_set.end()) {
+			if (iter->second != grammar->tag_any) {
+				Tag *value = single_tags[iter->second];
+				u_fprintf(output, "%S%S=%S>\n", stringbits[S_CMD_SETVAR].getTerminatedBuffer(), key->tag.c_str(), value->tag.c_str());
+			}
+			else {
+				u_fprintf(output, "%S%S>\n", stringbits[S_CMD_SETVAR].getTerminatedBuffer(), key->tag.c_str());
+			}
+		}
+		else {
+			u_fprintf(output, "%S%S>\n", stringbits[S_CMD_REMVAR].getTerminatedBuffer(), key->tag.c_str());
+		}
+	}
+
+	if (!window->text.empty()) {
+		u_fprintf(output, "%S", window->text.c_str());
+		if (!ISNL(window->text[window->text.length() - 1])) {
+			u_fputc('\n', output);
+		}
+	}
+
+	uint32_t cs = (uint32_t)window->cohorts.size();
+	for (uint32_t c = 0; c < cs; c++) {
+		Cohort *cohort = window->cohorts[c];
+		std::vector<Cohort*> cs = splitMwe(cohort);
+		foreach(iter, cs) {
+			printCohort(*iter, output);
+		}
+	}
+	u_fputc('\n', output);
+	u_fflush(output);
+}
+}
---
+++ src/MweSplitApplicator.hpp
@@ -0,0 +1,51 @@
+/*
+* Copyright (C) 2007-2016, GrammarSoft ApS
+* Developed by Tino Didriksen <mail@tinodidriksen.com>
+* Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
+*
+* This file is part of VISL CG-3
+*
+* VISL CG-3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* VISL CG-3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with VISL CG-3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef d01e916b65a71dd3_MWESPLITAPPLICATOR_HPP
+#define d01e916b65a71dd3_MWESPLITAPPLICATOR_HPP
+
+#include "GrammarApplicator.hpp"
+#include "Strings.hpp"
+#include "Tag.hpp"
+#include "Grammar.hpp"
+#include "Window.hpp"
+#include "SingleWindow.hpp"
+#include "Reading.hpp"
+#include "Cohort.hpp"
+
+namespace CG3 {
+
+class MweSplitApplicator : public virtual GrammarApplicator {
+private:
+	// bool did_warn_unhandled_situation;
+	const Tag* maybeWfTag(const Reading* r);
+	std::vector<Cohort*> splitMwe(Cohort* cohort);
+
+public:
+	MweSplitApplicator(UFILE *ux_err);
+	void runGrammarOnText(istream& input, UFILE *output);
+
+	void printSingleWindow(SingleWindow *window, UFILE *output);
+};
+}
+
+#endif
