93b9c04a7dd9b5630697f9d5f5751744610feb6d
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-08-01 13:15:37 +0000

    commentary, todo's
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@11703 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- emacs/cg.el
+++ emacs/cg.el
@@ -24,6 +24,13 @@
 
 ;;; Commentary:
 
+;; This package provides a major mode for editing Constraint Grammar
+;; source files, including syntax highlighting and interactive grammar
+;; development from within Emacs.  Use `C-c C-i' to edit the text you
+;; want to edit, then just `C-c C-c' whenever you want to run the
+;; grammar over that text.  Clicking on a line number in the trace
+;; output will take you to the definition of that rule.
+
 ;; Usage:
 ;;
 ;; (autoload 'cg-mode "/path/to/cg.el"
@@ -32,10 +39,10 @@
 ;; ; Or if you use a non-standard file suffix, e.g. .rlx:
 ;; (add-to-list 'auto-mode-alist '("\\.rlx\\'" . cg-mode))
 
-;; I recommend using company-mode for tab-completion, and
-;; smartparens-mode if you're used to it (paredit-mode does not work
-;; well if you have set names with the # character in them). Both are
-;; available from MELPA (see http://melpa.milkbox.net/).
+;; I recommend using `company-mode' for tab-completion, and
+;; `smartparens-mode' if you're used to it (`paredit-mode' does not
+;; work well if you have set names with the # character in them). Both
+;; are available from MELPA (see http://melpa.milkbox.net/).
 ;;
 ;; You can lazy-load company-mode for cg-mode like this:
 ;;
@@ -44,6 +51,7 @@
 
 
 ;; TODO:
+;; - investigate bug in `show-smartparens-mode' causing slowness
 ;; - different syntax highlighting for sets and tags (difficult)
 ;; - use something like prolog-clause-start to define M-a/e etc.
 ;; - run vislcg3 --show-unused-sets and buttonise with line numbers (like Occur does)
@@ -51,10 +59,7 @@
 ;; - the rest of the keywords
 ;; - http://beta.visl.sdu.dk/cg3/single/#regex-icase
 ;; - keyword tab-completion
-;; - the quotes-within-quotes thing plays merry hell with
-;;   paredit-doublequote, write a new doublequote function?
-;; - font-lock-syntactic-keywords is obsolete since 24.1
-;; - derive cg-mode from prog-mode?
+;; - `font-lock-syntactic-keywords' is obsolete since 24.1
 ;; - goto-set/list
 ;; - show definition of set/list-at-point in modeline
 ;; - show section name/number in modeline
