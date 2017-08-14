d20ec43957732eff9fc1a43d4578562702e7098c
Tino Didriksen <mail@tinodidriksen.com>
2017-08-14 16:40:18 +0000

    Drop big endian detection in favour of generic Boost
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@12305 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/Strings.hpp
+++ src/Strings.hpp
@@ -23,9 +23,11 @@
 #ifndef c6d28b7452ec699b_STRINGS_H
 #define c6d28b7452ec699b_STRINGS_H
 
+#include <cstdint>
+
 namespace CG3 {
 // ToDo: Add ABORT
-enum KEYWORDS {
+enum KEYWORDS : uint32_t {
 	K_IGNORE,
 	K_SETS,
 	K_LIST,
@@ -89,7 +91,7 @@ enum KEYWORDS {
 	KEYWORD_COUNT,
 };
 
-enum {
+enum : uint32_t {
 	S_IGNORE,
 	S_PIPE,
 	S_TO,
@@ -174,7 +176,7 @@ enum {
 };
 
 // This must be kept in lock-step with Rule.hpp's RULE_FLAGS
-enum {
+enum : uint32_t {
 	FL_NEAREST,
 	FL_ALLOWLOOP,
 	FL_DELAYED,
