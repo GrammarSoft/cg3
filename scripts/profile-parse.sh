d2f07bbb9bebd9aeec992f84831519fc6a2a8f19
Tino Didriksen <mail@tinodidriksen.com>
2017-09-18 13:03:15 +0000

    Profiling scripts
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@12371 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- scripts/profile-parse.sh
+++ scripts/profile-parse.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# 98 or 11
-export CXXV=11
 mkdir -p gprof
 mkdir -p gcov
 
@@ -8,10 +6,10 @@ svn up
 ./compile-profile.sh
 
 cd gcov/
-./vislcg3-c++$CXXV -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
+./vislcg3 -v0 -g ~/parsers/dansk/etc/dancg.cg --grammar-only --grammar-bin dancg.cg3b
 gcov -o . ../src/*.cpp >/dev/null 2>/dev/null
 gcov -o . ../src/TextualParser.cpp  >/dev/null 2>/dev/null
 
 cd ../gprof/
-./vislcg3-c++$CXXV -v0 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
+./vislcg3 -v0 -g ~/parsers/dansk/etc/dancg.cg --grammar-only --grammar-bin dancg.cg3b
 gprof vislcg3 >flat.txt
--- scripts/vparse.sh
+++ scripts/vparse.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# 98 or 11 or 1y
-CXXV=1y
 #svn up
 mkdir -p vparse
 cd vparse
 rm -f callgrind.*
 mv -vf annotated annotated.old
-g++ -std=c++$CXXV -DNDEBUG -Wall -Wextra -Wno-deprecated -pthread -pipe -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV.debug -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc
-valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3-c++$CXXV.debug -C UTF-8 --grammar-only -g ~/parsers/dansk/etc/dancg
+g++ -std=c++14 -DNDEBUG -Wall -Wextra -Wno-deprecated -pthread -pipe -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3.debug -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc
+valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug --grammar-only -g ~/parsers/dansk/etc/dancg.cg
 callgrind_annotate --tree=both --auto=yes > annotated
