c6ebcc67967930c4be5587770a8858407e9ec2fb
Tino Didriksen <mail@tinodidriksen.com>
2017-09-18 13:03:15 +0000

    Profiling scripts
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@12371 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- scripts/compile-profile.sh
+++ scripts/compile-profile.sh
@@ -2,12 +2,12 @@ mkdir -p gcov
 rm -fv gcov/*.gcov
 rm -fv gcov/*.gcda
 rm -fv gcov/*.gcno
-rm -fv gcov/vislcg3-c++$CXXV
+rm -fv gcov/vislcg3
 cd gcov
-g++ -std=c++$CXXV -DHAVE_BOOST -pthread -pipe -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -fprofile-arcs -ftest-coverage -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc -ltcmalloc
+g++ -std=c++14 -DHAVE_BOOST -pthread -pipe -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -fprofile-arcs -ftest-coverage -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3 -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc -ltcmalloc
 cd ..
 mkdir -p gprof
-rm -fv gprof/vislcg3-c++$CXXV
+rm -fv gprof/vislcg3
 rm -fv gprof/gmon.out
 cd gprof
-g++ -std=c++$CXXV -DHAVE_BOOST -pthread -pipe -pg -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc -ltcmalloc
+g++ -std=c++14 -DHAVE_BOOST -pthread -pipe -pg -Wall -Wextra -Wno-deprecated -Wno-unused-local-typedefs -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3 -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc -ltcmalloc
