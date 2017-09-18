936684f4d73911d54ae021abc90737df9a0abdab
Tino Didriksen <mail@tinodidriksen.com>
2017-09-18 13:03:15 +0000

    Profiling scripts
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@12371 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- scripts/profile-apply.sh
+++ scripts/profile-apply.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# 98 or 11
-export CXXV=11
 mkdir -p gprof
 mkdir -p gcov
 
 svn up
 make -j5
-./src/vislcg3 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
-./compile-profile.sh
+./src/vislcg3 -g ~/parsers/dansk/etc/dancg.cg --grammar-only --grammar-bin dancg.cg3b
+./scripts/compile-profile.sh
 
 cd gcov/
-#head -n 3000 arboretum_stripped.txt | ../src/vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out1.txt
-head -n 3000 arboretum_stripped.txt | ./vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out2.txt
+head -n 3000 arboretum_stripped.txt | ./vislcg3 -v -g ../dancg.cg3b > out.txt
 find ../src/ -type f -name '*.cpp' -exec gcov -o . {} \; >/dev/null 2>/dev/null
 gcov -o . ../src/GrammarApplicator_runRules.cpp  >/dev/null 2>/dev/null
 gcov -o . ../src/GrammarApplicator_matchSet.cpp  >/dev/null 2>/dev/null
 
 cd ../gprof/
-head -n 3000 arboretum_stripped.txt | ./vislcg3-c++$CXXV -v -C UTF-8 -g ../dancg.cg3b > out3.txt
+head -n 3000 arboretum_stripped.txt | ./vislcg3 -v -g ../dancg.cg3b > out.txt
 gprof vislcg3 >flat.txt
--- scripts/vapply.sh
+++ scripts/vapply.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# 98 or 11 or 1y
-CXXV=1y
 #svn up
 make -j5
-./src/vislcg3 -C UTF-8 -g ~/parsers/dansk/etc/dancg --grammar-only --grammar-bin dancg.cg3b
+./src/vislcg3 -g ~/parsers/dansk/etc/dancg.cg --grammar-only --grammar-bin dancg.cg3b
 mkdir -p vapply
 cd vapply
 rm -fv callgrind.*
 mv -vf annotated annotated.old
 mv -vf output.txt output.txt.old
-g++ -std=c++$CXXV -DNDEBUG -pthread -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3-c++$CXXV.debug -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc
-head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3-c++$CXXV.debug -v -t -C UTF-8 -g ../dancg.cg3b > output.txt
+g++ -std=c++14 -DNDEBUG -pthread -pipe -Wall -Wextra -Wno-deprecated -O3 -g3 -I../include -I../include/posix ../src/all_vislcg3.cpp -o vislcg3.debug -L/usr/lib/x86_64-linux-gnu -licui18n -licudata -licuio -licuuc
+head -n 2000 ../comparison/arboretum_stripped.txt | valgrind --tool=callgrind --compress-strings=no --compress-pos=no --collect-jumps=yes --collect-systime=yes ./vislcg3.debug -v -t -g ../dancg.cg3b > output.txt
 callgrind_annotate --tree=both --auto=yes > annotated
