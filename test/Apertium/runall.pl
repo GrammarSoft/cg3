b4a61378558ec35d089cfbf90491128601794ab2
Tino Didriksen <mail@tinodidriksen.com>
2020-04-01 14:50:18 +0000

    Handle Apertium prefixed tags, by Francis M. Tyers <ftyers@prompsit.com>
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/tools/vislcg3/trunk@14030 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- test/Apertium/runall.pl
+++ test/Apertium/runall.pl
@@ -9,6 +9,8 @@ chdir $bindir or die("Error: Could not change directory to $bindir !");
 
 # Search paths for the binary
 my @binlist = (
+	"../../build/VS16/src/Debug/vislcg3",
+	"../../build/VS16/src/Release/vislcg3",
 	"../Debug/vislcg3",
 	"../Common/vislcg3",
 	"../Release/vislcg3",
