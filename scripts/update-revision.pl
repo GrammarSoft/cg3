2b930905c23f1cd2fcf3ecad558fde5b708ea1a8
Tino Didriksen <mail@tinodidriksen.com>
2022-07-20 14:03:53 +0200

    Don't hardcode Perl path; Release 1.3.7

--- scripts/update-revision.pl
+++ scripts/update-revision.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
@@ -7,7 +7,7 @@ my $revision = `svnversion -n`;
 $revision =~ s/^([0-9]+).*/$1/g;
 $revision += 1;
 
-`/usr/bin/perl -e 's/CG3_REVISION = [0-9]+;\$/CG3_REVISION = $revision;/;' -pi src/version.hpp`;
+`perl -e 's/CG3_REVISION = [0-9]+;\$/CG3_REVISION = $revision;/;' -pi src/version.hpp`;
 `svn2cl --group-by-day -i --reparagraph --limit 350`;
 
 print "Set revision to $revision.\n";
