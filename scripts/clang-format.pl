ab2b6271373607a307e39f17d49d6e79c8f5827f
Tino Didriksen <mail@tinodidriksen.com>
2022-10-17 00:14:16 +0200

    clang-format update

--- scripts/clang-format.pl
+++ scripts/clang-format.pl
@@ -28,20 +28,21 @@ foreach my $file (@files) {
    my $data = file_read($file);
    $data =~ s@#pragma once\n#ifndef@PRAGMA_ONCE_IFNDEF@g;
    $data =~ s@([ \t]*#if.+?#endif\n)@// clang-format off\n$1// clang-format on\n@sg;
+   $data =~ s@([ \t]*#(?:include|define)[^\n]+\n)@// clang-format off\n$1// clang-format on\n@sg;
    $data =~ s@\n// clang-format on\n// clang-format off\n@\n@g;
    $data =~ s@PRAGMA_ONCE_IFNDEF@#pragma once\n#ifndef@g;
    file_write($file, $data);
 
-   `clang-format-6.0 -style=file -i '$file'`;
+   `clang-format -style=file -i '$file'`;
 
    my $data = file_read($file);
    $data =~ s@\n[^\n]*//[^\n]+clang-format (off|on)\n@\n@g; # Remove preprocessor protection
    # Things clang-format gets wrong:
-   $data =~ s@([ \t]+)(BOOST_FOREACH|boost_foreach|reverse_foreach|foreach)([^{\n]*) \{[ \t]+([^}\n]*)[ \t]+\}@$1$2$3 {\n$1\t$4\n$1}@g; # Don't allow single-line foreach blocks
-   $data =~ s@(operator .+?) \(@$1(@g; # No space before ( in operators
-   $data =~ s@ \*([,>)])@*$1@g; # No space before statement-final *
+   #$data =~ s@([ \t]+)(BOOST_FOREACH|boost_foreach|reverse_foreach|foreach)([^{\n]*) \{[ \t]+([^}\n]*)[ \t]+\}@$1$2$3 {\n$1\t$4\n$1}@g; # Don't allow single-line foreach blocks
+   #$data =~ s@(operator .+?) \(@$1(@g; # No space before ( in operators
+   #$data =~ s@ \*([,>)])@*$1@g; # No space before statement-final *
    $data =~ s@^([ \t]*)(  [:,] [^\n]+) \{@$1$2\n$1\{@mg; # { after ctor-init should go on its own line
-   $data =~ s@template <@template<@g; # No space in template<
+   #$data =~ s@template <@template<@g; # No space in template<
 
    # clang-format horribly mangles enums if AlignConsecutiveAssignments is off, so fix that
    my @enums = ($data =~ m@enum [^{]*\{(.+?)\n\s*\}[^;\n]*;@sg);
@@ -63,6 +64,7 @@ foreach my $file (@files) {
             $line =~ s@ = @$sps= @;
          }
          $line =~ s@\(1 << (\d)\)@(1 <<  $1)@;
+         $line =~ s@,\s+//@, //@;
          push @comb, $line;
       }
       my $comb = join "\n", @comb;
