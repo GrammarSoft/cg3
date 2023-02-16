50c14409240b181c63e52ecff47ed625bd1b5c8f
Tino Didriksen <mail@tinodidriksen.com>
2023-02-16 15:05:07 +0100

    Fix segfault for position self if scanning had gone beyond current window; Copyright year bump

--- src/version.hpp
+++ src/version.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2007-2021, GrammarSoft ApS
+* Copyright (C) 2007-2023, GrammarSoft ApS
 * Developed by Tino Didriksen <mail@tinodidriksen.com>
 * Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
 *
@@ -25,11 +25,11 @@
 
 #include <cstdint>
 
-constexpr auto CG3_COPYRIGHT_STRING = "Copyright (C) 2007-2021 GrammarSoft ApS. Licensed under GPLv3+";
+constexpr auto CG3_COPYRIGHT_STRING = "Copyright (C) 2007-2023 GrammarSoft ApS. Licensed under GPLv3+";
 
 constexpr uint32_t CG3_VERSION_MAJOR = 1;
 constexpr uint32_t CG3_VERSION_MINOR = 3;
-constexpr uint32_t CG3_VERSION_PATCH = 10;
+constexpr uint32_t CG3_VERSION_PATCH = 11;
 constexpr uint32_t CG3_REVISION = 13892;
 constexpr uint32_t CG3_FEATURE_REV = 13892;
 constexpr uint32_t CG3_TOO_OLD = 10373;
