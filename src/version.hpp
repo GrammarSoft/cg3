2a6c70f40f82513884ad3e67c3a7fc5453a826d7
Daniel Swanson <awesomeevildudes@gmail.com>
2023-03-04 19:52:45 -0500

    Contextual test positions for jumping to all the magic sets

--- src/version.hpp
+++ src/version.hpp
@@ -29,9 +29,9 @@ constexpr auto CG3_COPYRIGHT_STRING = "Copyright (C) 2007-2023 GrammarSoft ApS. 
 
 constexpr uint32_t CG3_VERSION_MAJOR = 1;
 constexpr uint32_t CG3_VERSION_MINOR = 4;
-constexpr uint32_t CG3_VERSION_PATCH = 0;
-constexpr uint32_t CG3_REVISION = 13894;
-constexpr uint32_t CG3_FEATURE_REV = 13894;
+constexpr uint32_t CG3_VERSION_PATCH = 1;
+constexpr uint32_t CG3_REVISION = 13895;
+constexpr uint32_t CG3_FEATURE_REV = 13895;
 constexpr uint32_t CG3_TOO_OLD = 10373;
 constexpr uint32_t CG3_EXTERNAL_PROTOCOL = 7226;
 
