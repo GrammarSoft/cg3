f890e75657c5c7043f0b5d1bafad3f11351cf3f0
Daniel Swanson <awesomeevildudes@gmail.com>
2025-07-02 18:44:24 -0400

    fix indentation

--- python/cg3.py
+++ python/cg3.py
@@ -7,16 +7,16 @@ from typing import DefaultDict, Dict, List, Optional
 
 @dataclass
 class Reading:
-    lemma: str = ''
-    tags: List[str] = field(default_factory=list)
+	lemma: str = ''
+	tags: List[str] = field(default_factory=list)
 	subreading: Optional[Reading] = None
 
 @dataclass
 class Cohort:
-    static: Reading = field(default_factory=Reading)
-    readings: List[Reading] = field(default_factory=list)
-    dep_self: int = 0
-    dep_head: Optional[int] = None
+	static: Reading = field(default_factory=Reading)
+	readings: List[Reading] = field(default_factory=list)
+	dep_self: int = 0
+	dep_head: Optional[int] = None
 	relations: DefalutDict[str, List[int]] = field(
 		default_factory=lambda: defaultdict(list))
 	text: str = ''
@@ -24,7 +24,7 @@ class Cohort:
 
 @dataclass
 class Window:
-    cohorts: List[Cohort] = field(default_factory=list)
+	cohorts: List[Cohort] = field(default_factory=list)
 	set_vars: Dict[str, Optional[str]] = field(default_factory=dict)
 	rem_vars: List[str] = field(default_factory=list)
 	text: str = ''
@@ -33,34 +33,34 @@ class Window:
 
 def parse_binary_window(buf):
 	pos = 0
-    def read_pat(pat):
-        nonlocal pos, buf
-        ret = struct.unpack_from('<'+pat, buf, pos)
-        pos += struct.calcsize('<'+pat)
-        return ret
-    def read_u16():
-        return read_pat('H')[0]
-    def read_u32():
-        return read_pat('I')[0]
-    def read_str():
-        l = read_u16()
-        if l == 0:
-            return b''
-        return read_pat(f'{l}s')[0]
-        return s.decode('utf-8')
+	def read_pat(pat):
+		nonlocal pos, buf
+		ret = struct.unpack_from('<'+pat, buf, pos)
+		pos += struct.calcsize('<'+pat)
+		return ret
+	def read_u16():
+		return read_pat('H')[0]
+	def read_u32():
+		return read_pat('I')[0]
+	def read_str():
+		l = read_u16()
+		if l == 0:
+			return b''
+		return read_pat(f'{l}s')[0]
+		return s.decode('utf-8')
 	window = Window()
-    window_flags = read_u16()
+	window_flags = read_u16()
 	if window_flags & 1:
 		window.flush_after = True
-    tag_count = read_u16()
-    tags = [read_str() for i in range(tag_count)]
-    def read_tags():
-        nonlocal tags
-        ct = read_u16()
-        if ct == 0:
-            return []
-        idx = read_pat(f'{ct}H')
-        return [tags[t] for t in idx]
+	tag_count = read_u16()
+	tags = [read_str() for i in range(tag_count)]
+	def read_tags():
+		nonlocal tags
+		ct = read_u16()
+		if ct == 0:
+			return []
+		idx = read_pat(f'{ct}H')
+		return [tags[t] for t in idx]
 	var_count = read_u16()
 	for i in range(var_count):
 		mode = read_pat('B')[0]
@@ -72,31 +72,31 @@ def parse_binary_window(buf):
 			window.set_vars[tags[t1]] = None
 		elif mode == 3:
 			window.rem_vars.append(tags[t1])
-    window.text = read_str()
-    window.text_post = read_str()
-    cohort_count = read_u16()
-    for i in range(cohort_count):
+	window.text = read_str()
+	window.text_post = read_str()
+	cohort_count = read_u16()
+	for i in range(cohort_count):
 		cohort = Cohort()
-        cohort_flags = read_u16()
+		cohort_flags = read_u16()
 		cohort.static.lemma = tags[read_u16()]
 		cohort.static.tags = read_tags()
-        cohort.dep_self = read_u32()
-        cohort.dep_parent = read_u32()
-        if cohort.dep_parent == 0xffffffff:
-            cohort.dep_parent = None
-        rel_count = read_u16()
-        for i in range(rel_count):
+		cohort.dep_self = read_u32()
+		cohort.dep_parent = read_u32()
+		if cohort.dep_parent == 0xffffffff:
+			cohort.dep_parent = None
+		rel_count = read_u16()
+		for i in range(rel_count):
 			tag = tags[read_u16()]
 			head = read_u32()
 			cohort.relations[tag].append(head)
 		cohort.text = read_str()
 		cohort.wblank = read_str()
-        reading_count = read_u16()
+		reading_count = read_u16()
 		prev = None
-        for i in range(reading_count):
-            reading_flags = read_u16()
+		for i in range(reading_count):
+			reading_flags = read_u16()
 			reading = Reading()
-            reading.lemma = tags[read_u16()]
+			reading.lemma = tags[read_u16()]
 			reading.tags = read_tags()
 			if reading_flags & 1 and prev is not None:
 				prev.subreading = reading
@@ -104,7 +104,7 @@ def parse_binary_window(buf):
 				cohort.readings.append(reading)
 			prev = reading
 		window.cohorts.append(cohort)
-    return window
+	return window
 
 def parse_binary_stream(fin):
 	header = fin.read(8)
