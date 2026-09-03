a027fe682c62f5466a3e0e69659e3312497b1779
Tino Didriksen <mail@tinodidriksen.com>
2026-09-03 14:13:22 +0200

    Bump copyright to 2026

--- src/JsonlApplicator.cpp
+++ src/JsonlApplicator.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2025, GrammarSoft ApS
+* Copyright (C) 2025-2026, GrammarSoft ApS
 * Developed by Tino Didriksen <mail@tinodidriksen.com>
 * Design by Eckhard Bick <eckhard.bick@mail.dk>, Tino Didriksen <mail@tinodidriksen.com>
 *
